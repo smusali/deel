413172a6b7fa8c2e99733ed664453fac23779e07
Samir Musali <samir.musali@gmail.com>
2023-02-16 21:29:44 +0200

    fix: fix the endpoint implementation

--- src/app.js
+++ src/app.js
@@ -11,11 +11,13 @@ app.set('models', sequelize.models)
  * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
+app.get('/contracts/:id', getProfile, async (req, res) =>{
+    const {Contract} = req && req.app && req.app.get('models')
+    const {id} = req && req.params
+
     const contract = await Contract.findOne({where: {id}})
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
+
 module.exports = app;
